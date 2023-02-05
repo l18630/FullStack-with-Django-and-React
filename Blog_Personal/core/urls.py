02e5f95e1acad1b7c2f2573b14eaa0a256bed2c0
l18630 <121523910+l18630@users.noreply.github.com>
2023-02-05 16:37:27 -0300

    Blog Avanzado

--- Blog_Personal/core/urls.py
+++ Blog_Personal/core/urls.py
@@ -13,22 +13,9 @@ Including another URLconf
     1. Import the include() function: from django.urls import include, path
     2. Add a URL to urlpatterns:  path('blog/', include('blog.urls'))
 """
-
 from django.contrib import admin
-from django.urls import path, re_path, include
-from django.views.generic import TemplateView
-from django.conf.urls.static import static
-from django.conf import settings
+from django.urls import path
 
-# https://docs.djangoproject.com/en/4.1/topics/http/urls/#how-django-processes-a-request
 urlpatterns = [
-
-    path('api/blog/', include('apps.blog.urls')),
-    path('api/category/', include('apps.category.urls')),
-
-    path('ckeditor/', include('ckeditor_uploader.urls')),
-
     path('admin/', admin.site.urls),
-] + static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
-
-urlpatterns += [re_path(r'^.*', TemplateView.as_view(template_name='index.html'))]
+]
